aa5caab56b744ed1df18c43eee65dfac37c914e1
Yunus ALTIN <yunusaltin1963@gmail.com>
2023-04-29 22:06:41 +0300

    Apache and Nginx-fpm-opcache

---
+++ docker/nginx-fpm-opcache/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction --ignore-platform-reqs
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo "env file exists."
+fi
+
+php artisan key:generate
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
+php artisan cache:clear
+
+role=${CONTAINER_ROLE:-app}
+
+if [ "$role" = "app" ]; then
+    php-fpm -D
+    nginx -g "daemon off;"
+elif [ "$role" = "queue" ]; then
+    # php /var/www/html/artisan horizon
+    
+    supervisord -n
+    supervisorctl start all
+fi
---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Vue template
+    </div>
+</template>
